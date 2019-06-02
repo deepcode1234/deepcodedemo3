37aab3c16c1c21fc5a64fcd68b31d979145453de
deepcode <deepcode123@163.com>
2019-06-02 23:26:29 +0800

    初次提交

---
+++ src/main/java/com/deepcode/Demo1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.deepcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sunny
+ *
+ */
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final List<Integer> nums = new ArrayList<Integer>();
+		for (int i = 0; i < 1000000; i++) {
+			nums.add(i);
+		}
+
+	}
+
+}
